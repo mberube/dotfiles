ff2b4bf0564711d6aa51d021bba242dcbc340937
Mathieu Berube <mathieu.berube@gmail.com>
2020-01-20 16:36:52 -0500

    Change command to save history on all iterm shells

--- bash/shell.sh
+++ bash/shell.sh
@@ -1,14 +1,15 @@
 # don't put duplicate lines in the history. See bash(1) for more options
 # ... or force ignoredups and ignorespace
-HISTCONTROL=ignoredups:ignorespace
+export HISTCONTROL=ignoredups:ignorespace
 
 # append to the history file, don't overwrite it
 shopt -s histappend
 
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
-HISTSIZE=10000
-HISTFILESIZE=20000
-PROMPT_COMMAND="history -a"
+export HISTSIZE=10000
+export HISTFILESIZE=20000
+# After each command, append to the history file and reread it
+export PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'n'}history -a; history -c; history -r"
 
 # check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
